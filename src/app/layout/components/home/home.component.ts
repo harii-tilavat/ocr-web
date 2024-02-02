7c111b817fcb035d54b735e5d216d3f4115c471c
harit <harit123@gmail.com>
2024-02-02 19:23:07 +0530

    added service data

--- src/app/layout/components/home/home.component.ts
+++ src/app/layout/components/home/home.component.ts
@@ -13,6 +13,32 @@ import { FileUploadService } from 'src/app/_services';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  public keyPoints = [
+    {
+      id: 1,
+      title: 'Upload Documents',
+      subTitle: 'Effortless Document Management',
+      desc: 'Easily upload various document formats, including Word and Excel files. Streamline your workflow with our intuitive document upload feature.',
+      imagePath: 'home-page-1',
+      subMenu: []
+    },
+    {
+      id: 2,
+      title: 'Automated Processing',
+      subTitle: 'Efficiency at its Best',
+      desc: 'Our system automatically processes and organizes your uploaded documents for efficient handling. Experience seamless automation to boost productivity.',
+      imagePath: 'home-page-2',
+      subMenu: []
+    },
+    {
+      id: 3,
+      title: 'Access Processed Data',
+      subTitle: 'Intuitive Data Retrieval',
+      desc: 'Retrieve and view the organized data through a user-friendly interface with search and filter capabilities. Effortlessly find and analyze your processed data.',
+      imagePath: 'home-page-3',
+      subMenu: []
+    },
+  ];
   public processDetail: Array<DocumentProccessDetail> = [
     {
       id: 1,
