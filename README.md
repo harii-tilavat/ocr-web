a4ce573d9de0a61f75f219f34b0f678f76dcee88
Harit tilavat <harittilavat10@gmail.com>
2024-04-13 15:47:19 +0530

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CuriousWeb
+# OCRWeb
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.5.
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,4 @@ export const environment = {
   production: false,
   baseUrl: 'http://localhost:8000',
   // baseUrl: 'https://ocrweb-node.onrender.com',
-  // apiUrl:'/api/v8/partner/documents'
-  // baseUrl: 'http://192.168.29.108:8607/v2'
-  // baseUrl: 'http://localhost:8607/v2' --testing
-  // baseUrl: 'https://api-curious.userstudy.co/v2'
 };
